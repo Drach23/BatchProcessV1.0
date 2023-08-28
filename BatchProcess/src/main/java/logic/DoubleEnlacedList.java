bbe47dd6c341c7e8520a5e0cf582320b4551de93
Drach <88101742+Drach23@users.noreply.github.com>
2023-08-28 00:04:54 -0600

    Modificaciones de la lista para imprimir

--- BatchProcess/src/main/java/logic/DoubleEnlacedList.java
+++ BatchProcess/src/main/java/logic/DoubleEnlacedList.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package logic;
 
-/**
- *
- * @author Richard
- */
 public class DoubleEnlacedList {
+    private int nextId = 1;
+    private String operation;
+    private Node head;
+    private Node tail;
     
+    
+    public Node getHead() {
+        return head;
+    }
+    
+    public void addToEnd(Process process) {
+        Node nodo = new Node(process);
+        if (head == null) {
+            head = tail = nodo;
+        } else {
+            nodo.prev = tail;
+            tail.next = nodo;
+            tail = nodo;
+        }
+    }
+
+    public void generateAndAddProcesses(int count) {
+        for (int i = 0; i < count; i++) {
+            String randomName = DataBank.generateRandomName();
+            String randomOperation = DataBank.generateRandomOperation();
+            int randomTme =DataBank.generateRandomTme();
+            Process proceso = new Process(nextId, randomName, randomTme, randomOperation);
+            addToEnd(proceso);
+            nextId++;
+        }
+    }
 }

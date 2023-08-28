f6c464a7290fae66198fb0fef1d373e0581cecd2
Drach <88101742+Drach23@users.noreply.github.com>
2023-08-28 00:05:28 -0600

    Modificaciones a nodo

--- BatchProcess/src/main/java/logic/Node.java
+++ BatchProcess/src/main/java/logic/Node.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package logic;
 
-/**
- *
- * @author Richard
- */
 public class Node {
+    public Process process;
+    public Node prev;
+    public Node next;
     
+    public Node(Process process){
+    this.process = process;
+  }
 }
